647dddaa77a69aecffe9aa9987f1f99af8d0501f
mcdd1024 <mcdd1024@outlook.com>
2024-07-08 21:00:39 +0800

    chore(demo03): init bean manage

---
+++ demo03-bean-manage-recommend-way/src/main/java/jzxy/cbq/Demo03BeanManageRecommendWayApplication.java
@@ -0,0 +1,20 @@
+package jzxy.cbq;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: cbq1024
+ * @description: Demo03BeanManageRecommendWayApplication
+ * @since 2024/7/8 下午8:58
+ */
+@SpringBootApplication
+@Slf4j
+public class Demo03BeanManageRecommendWayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Demo03BeanManageRecommendWayApplication.class, args);
+
+        log.info("Demo03BeanManageRecommendWayApplication run successful ");
+    }
+}
