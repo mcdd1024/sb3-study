d12881d12e384c30345f43120de1834bea07349e
mcdd1024 <mcdd1024@outlook.com>
2024-07-09 14:35:39 +0800

    chore(demo04): env

--- demo04-config-file/src/main/java/jzxy/cbq/Demo04ConfigFileApplication.java
+++ demo04-config-file/src/main/java/jzxy/cbq/Demo04ConfigFileApplication.java
@@ -3,10 +3,12 @@ package jzxy.cbq;
 import jzxy.cbq.entity.CBQ;
 import jzxy.cbq.entity.MySQLDataSource;
 import jzxy.cbq.entity.Redis;
+import jzxy.cbq.enums.LocalEnv;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.core.env.Environment;
 
 /**
  * @author: cbq1024
@@ -16,6 +18,7 @@ import org.springframework.context.ApplicationContext;
 @SpringBootApplication
 @Slf4j
 public class Demo04ConfigFileApplication {
+
     public static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(Demo04ConfigFileApplication.class, args);
 
@@ -26,6 +29,17 @@ public class Demo04ConfigFileApplication {
         System.out.println("dataSource = " + dataSource);
         System.out.println("redis = " + redis);
 
+        Environment environment = context.getBean(Environment.class);
+
+        String java_home = environment.getProperty(String.valueOf(LocalEnv.JAVA_HOME));
+        String mvn_home = environment.getProperty(String.valueOf(LocalEnv.MVN_HOME));
+        String gradle_home = environment.getProperty(String.valueOf(LocalEnv.GRADLE_HOME));
+        System.out.println("java_home = " + java_home);
+        System.out.println("maven_home = " + mvn_home);
+        System.out.println("gradle_home = " + gradle_home);
+
+
         log.info("Demo04ConfigFileApplication run successful");
     }
+
 }
---
+++ demo04-config-file/src/main/java/jzxy/cbq/enums/LocalEnv.java
@@ -0,0 +1,12 @@
+package jzxy.cbq.enums;
+
+/**
+ * @author: cbq1024
+ * @description: LocalEnv
+ * @since 2024/7/9 下午2:32
+ */
+public enum LocalEnv {
+    JAVA_HOME,
+    MVN_HOME,
+    GRADLE_HOME
+}
